1910400328fe31973d518cc42b0ae4c2baf1e090
walfonso <walfonso@hotmail.com>
2021-05-19 14:18:43 -0300

    Ejercicio 1.b

--- ejerciciosresueltos.js
+++ ejerciciosresueltos.js
@@ -7,3 +7,12 @@ var n1 = 10;
 var n2 = 20;
 var result = n1 + n2;
 console.log(result);
+
+/*
+	1.b) Crear dos variables de tipo String y concatenarlas guardando el resultado
+	en una 3er variable.
+*/
+var cadena1 = "Hola "; 
+var cadena2 = "Mundo";
+var result = cadena1 + cadena2;
+console.log(result);
