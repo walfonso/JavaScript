ddc5aa492773f50546353f59ebaea7b12ffbf746
walfonso <walfonso@hotmail.com>
2021-05-19 15:25:22 -0300

    Ejercicio 3.f

--- ejerciciosresueltos.js
+++ ejerciciosresueltos.js
@@ -140,3 +140,12 @@ var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
 meses.reverse();
 console.log(meses);
+
+/*
+	3.f) Unir todos los elementos del array en un único string donde cada mes este
+	separado por un guión - (utilizar join).
+*/
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
+var result = meses.join("-");
+console.log(result);
