ba21272cf98202ad2bc93d74c9cc64bac87a81a9
walfonso <walfonso@hotmail.com>
2021-05-19 19:45:45 -0300

    Arreglos endentado

--- ejerciciosresueltos.js
+++ ejerciciosresueltos.js
@@ -1,7 +1,7 @@
 /*
-  1) Variables y Operadores
-	a) Crear dos variables numéricas y utilizar el operador suma para guardar
-	el valor de la suma de ambos números en una 3er variable.
+1) Variables y Operadores
+a) Crear dos variables numéricas y utilizar el operador suma para guardar
+el valor de la suma de ambos números en una 3er variable.
 */
 var n1 = 10;
 var n2 = 20;
@@ -9,8 +9,8 @@ var result = n1 + n2;
 console.log(result);
 
 /*
-	1.b) Crear dos variables de tipo String y concatenarlas guardando el resultado
-	en una 3er variable.
+1.b) Crear dos variables de tipo String y concatenarlas guardando el resultado
+en una 3er variable.
 */
 var cadena1 = "Hola "; 
 var cadena2 = "Mundo";
@@ -18,9 +18,9 @@ var result = cadena1 + cadena2;
 console.log(result);
 
 /*
-	1.c) Crear dos variables de tipo String y sumar el largo de cada variable 
-	(cantidad de letras del string) guardando el resultado la suma una 3er variable 
-	(utilizar length).
+1.c) Crear dos variables de tipo String y sumar el largo de cada variable 
+(cantidad de letras del string) guardando el resultado la suma una 3er variable 
+(utilizar length).
 */
 var cadena1 = "Hola"; 
 var cadena2 = "Mundo";
@@ -28,27 +28,27 @@ var result = cadena1.length + cadena2.length;
 console.log(result);
 
 /*
-	2) Strings
-	a) Crear una variable de tipo string con al menos 10 caracteres y convertir todo 
-	el texto en mayúscula (utilizar toUpperCase).
+2) Strings
+a) Crear una variable de tipo string con al menos 10 caracteres y convertir todo 
+el texto en mayúscula (utilizar toUpperCase).
 */
 var cadena = "Hola mundo!!";
 cadena = cadena.toUpperCase();
 console.log(cadena);
 
 /*
-	2.b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
-	string con los primeros 5 caracteres guardando el resultado en una nueva variable 
-	(utilizar substring).
+2.b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
+string con los primeros 5 caracteres guardando el resultado en una nueva variable 
+(utilizar substring).
 */
 var cadena = "programacion";
 var result = cadena.substring(0,5);
 console.log(result);
 
 /*
-	2.c) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
-	string con los últimos 3 caracteres guardando el resultado en una nueva variable 
-	(utilizar substring).
+2.c) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
+string con los últimos 3 caracteres guardando el resultado en una nueva variable 
+(utilizar substring).
 */
 var cadena = "programacion";
 var result = cadena.substring(9,12);
@@ -60,30 +60,30 @@ var result = cadena.substring(cadena.length-3,cadena.length);
 console.log("Aplica Mejora: "+result)
 
 /*
-	2.d) Crear una variable de tipo string con al menos 10 caracteres y generar un 
-	nuevo string con la primera letra en mayúscula y las demás en minúscula. 
-	Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, 
-	toLowerCase y el operador +).
+2.d) Crear una variable de tipo string con al menos 10 caracteres y generar un 
+nuevo string con la primera letra en mayúscula y las demás en minúscula. 
+Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, 
+toLowerCase y el operador +).
 */
 var cadena = "eSTRUCTURAS";
 var result = cadena.substring(0,1).toUpperCase() + cadena.substring(1,).toLowerCase();
 console.log(result);
 
 /*
-	2.e) Crear una variable de tipo string con al menos 10 caracteres y algún espacio 
-	en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una 
-	variable (utilizar indexOf).
+2.e) Crear una variable de tipo string con al menos 10 caracteres y algún espacio 
+en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una 
+variable (utilizar indexOf).
 */
 var cadena = "Prog ramas POO";
 var result = cadena.indexOf(" ");
 console.log("Posición: "+result);
 
 /*
-	2.f) Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres 
-	y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para 
-	generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y
-	las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y 
-	el operador +).
+2.f) Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres 
+y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para 
+generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y
+las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y 
+el operador +).
 */
 var cadena = "pROGRAMACION eSTRUCTURADA";
 var posicion = cadena.indexOf(" ");
@@ -93,10 +93,10 @@ var result = palabra1 + palabra2;
 console.log(result);
 
 /*
-	3) Arrays
-	a) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
-	"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y
-	11 (utilizar console.log)
+3) Arrays
+a) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y
+11 (utilizar console.log)
 */
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
@@ -105,18 +105,17 @@ var mes11 = meses[10];
 console.log("Mes 5: " + mes5 + " " + "Mes 11: " + mes11);
 
 /*	
-	3.b) Ordenar el array de meses alfabéticamente y mostrarlo
-	por consola (utilizar sort).
+3.b) Ordenar el array de meses alfabéticamente y mostrarlo
+por consola (utilizar sort).
 */
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
 meses.sort();
 console.log(meses);
 
-
 /*
-	3.c) Agregar un elemento al principio y al final del array 
-	(utilizar unshift y push).
+3.c) Agregar un elemento al principio y al final del array 
+(utilizar unshift y push).
 */
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
@@ -125,7 +124,7 @@ meses.push('finalMes');
 console.log(meses)
 
 /*	
-	3.d) Quitar un elemento del principio y del final del array (utilizar shift y pop).
+3.d) Quitar un elemento del principio y del final del array (utilizar shift y pop).
 */
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
@@ -134,7 +133,7 @@ meses.pop();
 console.log(meses);
 
 /*	
-	3.e) Invertir el orden del array (utilizar reverse)
+3.e) Invertir el orden del array (utilizar reverse)
 */	
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
@@ -142,8 +141,8 @@ meses.reverse();
 console.log(meses);
 
 /*
-	3.f) Unir todos los elementos del array en un único string donde cada mes este
-	separado por un guión - (utilizar join).
+3.f) Unir todos los elementos del array en un único string donde cada mes este
+separado por un guión - (utilizar join).
 */
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
@@ -151,8 +150,8 @@ var result = meses.join("-");
 console.log(result);
 
 /*
-	3.g) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre 
-	(utilizar slice).
+3.g) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre 
+(utilizar slice).
 */
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] ;
@@ -250,7 +249,7 @@ for (var i = 0; i < 10; i++){
 }
 console.log(miArreglo);
 
-/**
+/*
  6) Funciones
 */
 
