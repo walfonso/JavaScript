3aba636c73ecae24fc028b22e8921a68089b2181
walfonso <walfonso@hotmail.com>
2021-05-19 14:15:13 -0300

    Ejercicio 1.a

---
+++ ejerciciosresueltos.js
@@ -0,0 +1,9 @@
+/*
+  1) Variables y Operadores
+	a) Crear dos variables numéricas y utilizar el operador suma para guardar
+	el valor de la suma de ambos números en una 3er variable.
+*/
+var n1 = 10;
+var n2 = 20;
+var result = n1 + n2;
+console.log(result);
