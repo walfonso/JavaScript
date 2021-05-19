db6f2b22bd7f686b61aa5677fd2260ee6450d47b
walfonso <walfonso@hotmail.com>
2021-05-19 15:02:25 -0300

    Ejercicio 2.b

--- ejerciciosresueltos.js
+++ ejerciciosresueltos.js
@@ -36,5 +36,14 @@ var cadena = "Hola mundo!!";
 cadena = cadena.toUpperCase();
 console.log(cadena);
 
+/*
+	2.b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
+	string con los primeros 5 caracteres guardando el resultado en una nueva variable 
+	(utilizar substring).
+*/
+var cadena = "programacion";
+var result = cadena.substring(0,5);
+console.log(result);
+
 
 
